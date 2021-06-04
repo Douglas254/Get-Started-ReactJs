60d35a44d890a680f64440b6ff02e3f3a6ee961d
Douglas Obara <dobara@kabarak.ac.ke>
2021-06-04 17:41:02 +0300

    Named and Default exports in React library

--- src/Modules/index.js
+++ src/Modules/index.js
@@ -1,4 +1,8 @@
-import { Teacher } from "./teacher";
+import Teacher, { promote } from "./teacher";
+import React, { Component } from 'react';
+// Named and Default exports 
+// Default -> import ... from ''; e.g React
+// Named -> import {...} from ''; e.g Component
 
 const teacher = new Teacher("Dougla","BSc");
 teacher.teach();
